95a145e33ea931fa320a122b19bffad47eb355c3
Dungx287 <manhdung28797@gmail.com>
2022-07-20 09:48:54 +0700

    clear number

--- src/ClassSelectionMenu/index.js
+++ src/ClassSelectionMenu/index.js
@@ -63,15 +63,7 @@ export const ClassSelectionMenu = ({
     for (let i = 0; i < 9 && i < regionClsList.length; i++) {
       keyMapping[i + 1] = () => onSelectCls(regionClsList[i])
     }
-    const onKeyDown = (e) => {
-      if (keyMapping[e.key]) {
-        keyMapping[e.key]()
-        e.preventDefault()
-        e.stopPropagation()
-      }
-    }
-    window.addEventListener("keydown", onKeyDown)
-    return () => window.removeEventListener("keydown", onKeyDown)
+
   }, [regionClsList, selectedCls])
 
   return (
@@ -94,9 +86,6 @@ export const ClassSelectionMenu = ({
               {capitalize(label)}
             </Label>
             <DashSep />
-            <Number className={classnames({ selected: label === selectedCls })}>
-              {index < 9 ? `Key [${index + 1}]` : ""}
-            </Number>
           </LabelContainer>
         ))}
         <Box pb={2} />
